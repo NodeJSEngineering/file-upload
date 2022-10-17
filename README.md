0ef5bc22099be0880d2f7b4dfaf58171cfc887e2
Sunny Gupta <sunny.gupta@globallogic.com>
2022-10-17 21:53:11 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Upload Single/Multiple Files in Node.js
 <p align="center"><img src="https://github.com/ultimateakash/node-file-upload/blob/master/public/images/node-file-upload.png"></p> 
+
+npm start
--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -14,11 +14,52 @@ exports.uploadSingle = (req, res) => {
     return res.redirect('/');
 }
 
-exports.uploadMultiple = (req, res) => {
+exports.uploadMultiple = async (req, res) => {
     if (req.files.length) {
         console.log(req.files)
+     
+         
 
-        req.flash('success', 'Files Uploaded.');
+        const PDFkitDocument = require('pdfkit');
+        let fs = require('fs');
+        const PDFLibDocument = require('pdf-lib').PDFDocument;
+        const doc = new PDFkitDocument();
+        req.files.forEach((file) => {
+            doc.image(file.path, {
+                align: 'center',
+                valign: 'center'
+            });
+            doc
+        .addPage()
+          });
+          doc.pipe(fs.createWriteStream('example1.pdf'));
+          doc.end();
+        //   const allFilesBuffer = []
+        //   req.files.forEach(async (file) => {
+        //     allFilesBuffer.push(fs.readFileSync(file.path));
+        //   });
+         
+        // const mergedPdf = await PDFLibDocument.create(); 
+        // for (const pdfBytes of allFilesBuffer) { 
+        //     const pdf = await PDFLibDocument.load(pdfBytes); 
+        //     const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+        //     copiedPages.forEach((page) => {
+        //          mergedPdf.addPage(page); 
+        //     }); 
+        // } 
+        
+        // const buf = await mergedPdf.save();        // Uint8Array
+        
+        // let path = 'merged.pdf'; 
+        // fs.open(path, 'w', function (err, fd) {
+        //     fs.write(fd, buf, 0, buf.length, null, function (err) {
+        //         fs.close(fd, function () {
+        //             console.log('wrote the file successfully');
+        //         }); 
+        //     }); 
+        // }); 
+
+        // req.flash('success', 'Files Uploaded.');
     }
     return res.redirect('/');
 }
